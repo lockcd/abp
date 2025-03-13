fca845bd6772d5113dc923dc1f79985ab44a63b5
erdemcaygor <erdem.caygor@gmail.com>
2025-03-13 14:45:13 +0300

    code refactoring

--- npm/ng-packs/packages/components/extensible/src/lib/components/multi-select/extensible-form-multiselect.component.ts
+++ npm/ng-packs/packages/components/extensible/src/lib/components/multi-select/extensible-form-multiselect.component.ts
@@ -1,7 +1,6 @@
 import { Component, ChangeDetectionStrategy, forwardRef, input } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 import { ABP, LocalizationModule } from '@abp/ng.core';
 import { BaseThemeSharedModule } from '@abp/ng.theme.shared';
 import { FormProp } from '@abp/ng.components/extensible';
@@ -17,21 +16,19 @@ const EXTENSIBLE_FORM_MULTI_SELECT_CONTROL_VALUE_ACCESSOR = {
   template: `
     <div [id]="prop().id">
       @for (option of options(); track option.value) {
-        <div class="d-flex align-items-center column-gap-2">
-          <div class="form-check" validationTarget>
-            <input
-              type="checkbox"
-              class="form-check-input"
-              [disabled]="disabled"
-              [checked]="isChecked(option.value)"
-              (change)="onCheckboxChange(option.value, $event)"
-            />
-            @if (prop().isExtra) {
-              {{ '::' + option.key | abpLocalization }}
-            } @else {
-              {{ option.key }}
-            }
-          </div>
+        <div class="form-check" validationTarget>
+          <input
+            type="checkbox"
+            class="form-check-input"
+            [disabled]="disabled"
+            [checked]="isChecked(option.value)"
+            (change)="onCheckboxChange(option.value, $event)"
+          />
+          @if (prop().isExtra) {
+            {{ '::' + option.key | abpLocalization }}
+          } @else {
+            {{ option.key }}
+          }
         </div>
       }
     </div>
