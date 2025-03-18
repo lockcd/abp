2f6b8c9c306cc83d23be949ff733451ade928158
maliming <6908465+maliming@users.noreply.github.com>
2025-03-18 17:44:02 +0800

    Fix Studio files in VirtualFileExplorer module.

--- modules/virtual-file-explorer/app/Migrations/20250315060727_Initial.cs
+++ modules/virtual-file-explorer/app/Migrations/20250315060727_Initial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace DempApp.Migrations
+namespace DemoApp.Migrations
 {
     /// <inheritdoc />
     public partial class Initial : Migration
