a7208935947e27d33c183e9e6490eb91a367f45a
liangshiwei <deawback@hotmail.com>
2025-04-21 16:32:12 +0800

    Clear Title while disposing PageHeader

--- framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Layout/PageHeader.razor.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Layout/PageHeader.razor.cs
@@ -113,6 +113,7 @@ public partial class PageHeader : ComponentBase, IDisposable
     {
         PageLayout.ToolbarItems.Clear();
         PageLayout.BreadcrumbItems.Clear();
+        PageLayout.Title = string.Empty;
         ToolbarItemRenders.Clear();
     }
 }
