40bd4b2fc5a51c56435cf5bb77395bcd620ec974
maliming <malimings@gmail.com>
2025-04-30 13:14:08 +0800

    Add `ITenantValidator`.

--- modules/tenant-management/src/Volo.Abp.TenantManagement.Domain/Volo/Abp/TenantManagement/ITenantNameValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace Volo.Abp.TenantManagement;
-
-public interface ITenantNameValidator
-{
-    Task ValidateAsync(string normalizedTenantName, Guid? expectedTenantId = null);
-}
--- modules/tenant-management/src/Volo.Abp.TenantManagement.Domain/Volo/Abp/TenantManagement/TenantManager.cs
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Domain/Volo/Abp/TenantManagement/TenantManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Volo.Abp.Caching;
+﻿using System.Threading.Tasks;
 using Volo.Abp.Domain.Services;
 using Volo.Abp.EventBus.Local;
 using Volo.Abp.MultiTenancy;
@@ -9,27 +7,25 @@ namespace Volo.Abp.TenantManagement;
 
 public class TenantManager : DomainService, ITenantManager
 {
-    public ITenantNameValidator TenantNameValidator { get; }
+    protected ITenantValidator TenantValidator { get; }
     protected ITenantNormalizer TenantNormalizer { get; }
     protected ILocalEventBus LocalEventBus { get; }
 
     public TenantManager(
-        ITenantNameValidator tenantNameValidator,
+        ITenantValidator tenantValidator,
         ITenantNormalizer tenantNormalizer,
         ILocalEventBus localEventBus)
     {
-        TenantNameValidator = tenantNameValidator;
+        TenantValidator = tenantValidator;
         TenantNormalizer = tenantNormalizer;
         LocalEventBus = localEventBus;
     }
 
     public virtual async Task<Tenant> CreateAsync(string name)
     {
-        Check.NotNull(name, nameof(name));
-
-        var normalizedName = TenantNormalizer.NormalizeName(name);
-        await TenantNameValidator.ValidateAsync(normalizedName);
-        return new Tenant(GuidGenerator.Create(), name, normalizedName);
+        var tenant = new Tenant(GuidGenerator.Create(), name, TenantNormalizer.NormalizeName(name));
+        await TenantValidator.ValidateAsync(tenant);
+        return tenant;
     }
 
     public virtual async Task ChangeNameAsync(Tenant tenant, string name)
@@ -37,11 +33,10 @@ public class TenantManager : DomainService, ITenantManager
         Check.NotNull(tenant, nameof(tenant));
         Check.NotNull(name, nameof(name));
 
-        var normalizedName = TenantNormalizer.NormalizeName(name);
-
-        await TenantNameValidator.ValidateAsync(normalizedName, tenant.Id);
         await LocalEventBus.PublishAsync(new TenantChangedEvent(tenant.Id, tenant.NormalizedName));
+
         tenant.SetName(name);
-        tenant.SetNormalizedName(normalizedName);
+        tenant.SetNormalizedName( TenantNormalizer.NormalizeName(name));
+        await TenantValidator.ValidateAsync(tenant);
     }
 }
