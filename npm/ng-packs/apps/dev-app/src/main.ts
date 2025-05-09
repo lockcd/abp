c4843f6b026ceec8db7bf626806c76a50c79c3bf
erdemcaygor <erdem.caygor@gmail.com>
2025-05-09 16:21:57 +0300

    dev app config updated

--- npm/ng-packs/apps/dev-app/src/main.ts
+++ npm/ng-packs/apps/dev-app/src/main.ts
@@ -1,43 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
-
-import { environment } from './environments/environment';
-import { APP_ROUTE_PROVIDER } from './app/route.provider';
-import { provideAbpCore, withOptions } from '@abp/ng.core';
-import { registerLocale } from '@abp/ng.core/locale';
-import { provideAbpOAuth } from '@abp/ng.oauth';
-import { provideAbpThemeShared } from '@abp/ng.theme.shared';
-import { provideSettingManagementConfig } from '@abp/ng.setting-management/config';
-import { provideAccountConfig } from '@abp/ng.account/config';
-import { provideIdentityConfig } from '@abp/ng.identity/config';
-import { provideTenantManagementConfig } from '@abp/ng.tenant-management/config';
-import { provideFeatureManagementConfig } from '@abp/ng.feature-management';
-import { appRoutes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes),
-    APP_ROUTE_PROVIDER,
-    provideAbpCore(
-      withOptions({
-        environment,
-        registerLocaleFn: registerLocale(),
-        sendNullsAsQueryParam: false,
-        skipGetAppConfiguration: false,
-      }),
-    ),
-    provideAbpOAuth(),
-    provideAbpThemeShared(),
-    provideSettingManagementConfig(),
-    provideAccountConfig(),
-    provideIdentityConfig(),
-    provideTenantManagementConfig(),
-    provideFeatureManagementConfig(),
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
