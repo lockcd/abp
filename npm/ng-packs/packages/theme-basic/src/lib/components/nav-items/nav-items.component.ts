26a4e6aa5d192fea275ce0c094e151f7292e1456
erdemcaygor <erdem.caygor@gmail.com>
2025-05-23 12:20:03 +0300

    fix

--- npm/ng-packs/packages/theme-basic/src/lib/components/nav-items/nav-items.component.ts
+++ npm/ng-packs/packages/theme-basic/src/lib/components/nav-items/nav-items.component.ts
@@ -1,12 +1,12 @@
 import { AbpVisibleDirective, NavItem, NavItemsService } from '@abp/ng.theme.shared';
 import { Component, TrackByFunction } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PermissionDirective } from '@abp/ng.core';
+import { PermissionDirective, ToInjectorPipe } from '@abp/ng.core';
 
 @Component({
   selector: 'abp-nav-items',
   templateUrl: 'nav-items.component.html',
-  imports: [CommonModule, AbpVisibleDirective, PermissionDirective],
+  imports: [CommonModule, AbpVisibleDirective, PermissionDirective, ToInjectorPipe],
 })
 export class NavItemsComponent {
   trackByFn: TrackByFunction<NavItem> = (_, element) => element.id;
