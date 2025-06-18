a8059260d7c1ea24d217073a6ba1d4113c2cc178
maliming <malimings@gmail.com>
2025-06-18 16:35:41 +0800

    Refactor JQueryValidationScriptContributor to conditionally add validation script

--- npm/packs/jquery-validation/abp.resourcemapping.js
+++ npm/packs/jquery-validation/abp.resourcemapping.js
@@ -3,6 +3,5 @@ module.exports = {
         "@node_modules/jquery-validation/dist/jquery.validate.js": "@libs/jquery-validation/",
         "@node_modules/jquery-validation/dist/localization/*.*": "@libs/jquery-validation/localization/",
         "@node_modules/@abp/jquery-validation/src/*.*": "@libs/jquery-validation/"
-
     }
 }
