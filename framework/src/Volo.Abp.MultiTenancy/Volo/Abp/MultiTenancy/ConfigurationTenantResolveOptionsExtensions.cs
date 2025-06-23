b595965bf9253fbe79425fa6992a038936c54617
Suhaib Mousa <suhaibmousa032@gmail.com>
2025-06-24 00:40:44 +0300

    Adds a new tenant resolve contributor

---
+++ framework/src/Volo.Abp.MultiTenancy/Volo/Abp/MultiTenancy/ConfigurationTenantResolveOptionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Volo.Abp.MultiTenancy;
+
+public static class ConfigurationTenantResolveOptionsExtensions
+{
+    public static void AddConfigurationTenantResolver(
+        this AbpTenantResolveOptions options)
+    {
+        options.TenantResolvers.InsertAfter(
+            r => r is CurrentUserTenantResolveContributor,
+            new ConfigurationTenantResolveContributor()
+        );
+    }
+}
