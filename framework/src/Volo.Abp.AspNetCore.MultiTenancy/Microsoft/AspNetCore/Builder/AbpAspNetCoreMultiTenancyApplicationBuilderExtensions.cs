6a92781d107ff9fe91146540f7d15f1164086293
maliming <malimings@gmail.com>
2025-07-08 17:44:47 +0800

    Warn if MultiTenancyMiddleware is used before authentication

--- framework/src/Volo.Abp.AspNetCore.MultiTenancy/Microsoft/AspNetCore/Builder/AbpAspNetCoreMultiTenancyApplicationBuilderExtensions.cs
+++ framework/src/Volo.Abp.AspNetCore.MultiTenancy/Microsoft/AspNetCore/Builder/AbpAspNetCoreMultiTenancyApplicationBuilderExtensions.cs
@@ -1,12 +1,24 @@
-﻿using Volo.Abp.AspNetCore.MultiTenancy;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.AspNetCore.MultiTenancy;
 
 namespace Microsoft.AspNetCore.Builder;
 
 public static class AbpAspNetCoreMultiTenancyApplicationBuilderExtensions
 {
+    private const string AuthenticationMiddlewareSetKey = "__AuthenticationMiddlewareSet";
+
+
     public static IApplicationBuilder UseMultiTenancy(this IApplicationBuilder app)
     {
-        return app
-            .UseMiddleware<MultiTenancyMiddleware>();
+        var authenticationMiddlewareSet = app.Properties.TryGetValue(AuthenticationMiddlewareSetKey, out var value) && value is true;
+        if (!authenticationMiddlewareSet)
+        {
+            var logger = app.ApplicationServices.GetService<ILogger<MultiTenancyMiddleware>>();
+            logger?.LogWarning("MultiTenancyMiddleware is being registered before the authentication middleware. " +
+                               "This may lead to incorrect tenant resolution. " +
+                               "Ensure that app.UseAuthentication() is called before app.UseMultiTenancy().");
+        }
+        return app.UseMiddleware<MultiTenancyMiddleware>();
     }
 }
