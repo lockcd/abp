aecc051fcbfffbb53c03c1def79bcf291947d19f
maliming <malimings@gmail.com>
2025-07-15 16:30:08 +0800

    Add custom error codes for account lock and inactivity

--- modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/Controllers/TokenController.Password.cs
+++ modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/Controllers/TokenController.Password.cs
@@ -107,14 +107,14 @@ public partial class TokenController
                         ClientId = request.ClientId
                     });
 
+                    var errorCode = OpenIddictConstants.Errors.InvalidGrant;
                     string errorDescription;
-                    string errorCode;
-                    
+
                     if (result.IsLockedOut)
                     {
                         Logger.LogInformation("Authentication failed for username: {username}, reason: locked out", request.Username);
+                        errorCode = AbpOpenIddictErrors.AccountLocked;
                         errorDescription = "The user account has been locked out due to invalid login attempts. Please wait a while and try again.";
-                        errorCode = "account_locked";
                     }
                     else if (result.IsNotAllowed)
                     {
@@ -126,7 +126,6 @@ public partial class TokenController
                         else
                         {
                             Logger.LogInformation("Authentication failed for username: {username}, reason: not allowed", request.Username);
-
 
                             if (user.ShouldChangePasswordOnNextLogin)
                             {
@@ -143,15 +142,14 @@ public partial class TokenController
                                 return await HandleConfirmUserAsync(request, user);
                             }
 
+                            errorCode = AbpOpenIddictErrors.AccountInactive;
                             errorDescription = "You are not allowed to login! Your account is inactive or needs to confirm your email/phone number.";
-                            errorCode = "account_inactive";
                         }
                     }
                     else
                     {
                         Logger.LogInformation("Authentication failed for username: {username}, reason: invalid credentials", request.Username);
                         errorDescription = "Invalid username or password!";
-                        errorCode = OpenIddictConstants.Errors.InvalidGrant;
                     }
 
                     var properties = new AuthenticationProperties(new Dictionary<string, string>
