b9bc4f5dbdce2c24190ccf59da66326405c00d72
maliming <malimings@gmail.com>
2025-07-28 10:04:21 +0800

    Update LoggedOut.js

--- modules/account/src/Volo.Abp.Account.Web/Pages/Account/LoggedOut.js
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/LoggedOut.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    setTimeout(function () {
+    var timer = setTimeout(function () {
         var redirectButton = document.getElementById("redirectButton");
-        
-        if(!redirectButton){
+        if (!redirectButton) {
+            clearTimeout(timer);
             return;
         }
-            
-        redirectButton.getAttribute("cname");
-        redirectButton.getAttribute("href");
-    }, 3000)
+        var clientName = redirectButton.getAttribute("cname");
+        if (!clientName) {
+            window.clientName = clientName;
+        }
+        var href = redirectButton.getAttribute("href");
+        if (!href) {
+            clearTimeout(timer);
+            return;
+        }
+        window.location = href;
+        clearTimeout(timer);
+    }, 3000);
 });
