de1db9761a909d3757bd4459919c55808f767691
sumeyye <sum.the.developer@gmail.com>
2025-07-30 09:32:16 +0300

    update: remove unused imports

--- npm/ng-packs/packages/core/src/lib/providers/core-module-config.provider.ts
+++ npm/ng-packs/packages/core/src/lib/providers/core-module-config.provider.ts
@@ -1,4 +1,4 @@
-import { makeEnvironmentProviders, Provider, provideAppInitializer, inject, Injector } from '@angular/core';
+import { makeEnvironmentProviders, Provider, provideAppInitializer, inject } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
 import {
   HTTP_INTERCEPTORS,
@@ -23,7 +23,7 @@ import { ABP, SortableItem } from '../models';
 import { AuthErrorFilterService } from '../abstracts';
 import { DEFAULT_DYNAMIC_LAYOUTS } from '../constants';
 import { LocalizationService, LocalStorageListenerService, AbpTitleStrategy } from '../services';
-import { DefaultQueueManager, getInitialData, localeInitializer } from '../utils';
+import { DefaultQueueManager, getInitialData } from '../utils';
 import { CookieLanguageProvider, IncludeLocalizationResourcesProvider, LocaleProvider } from './';
 import { TimezoneInterceptor } from '../interceptors';
 
